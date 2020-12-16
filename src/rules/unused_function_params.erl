6f62eeeb3a17089b96411e5ff08e32c20eeda914
Diego Calero <diego.calero@adroll.com>
2020-12-16 15:46:47 -0300

    Making the rule to work.

--- src/rules/unused_function_params.erl
+++ src/rules/unused_function_params.erl
@@ -6,41 +6,59 @@
 
 -spec analyze(hank_rule:asts(), hank_context:t()) -> [hank_rule:result()].
 analyze(ASTs, _Context) ->
-    [Result
+    [Results
      || {File, AST} <- ASTs,
         Function <- [Node || Node <- AST, erl_syntax:type(Node) == function],
-        Result <- [set_result(File, check_function(Function))],
-        Result =/= ok].
+        Results <- analyze_function(File, Function)].
+
+%% @doc It will check if arguments are ignored in all function clauses:
+%%      [(_a, b, _c), (_x, b, c)]
+%%      [[1, 0, 1], [1, 0, 0]] => [1, 0, 0] => warning 1st param!
+%%      [(a, _b, c), (_, b, c)]
+%%      [[0, 1, 0], [1, 0, 0]] => [0, 0, 0] => ok
+analyze_function(File, Function) ->
+    lists:foldl(fun(Result, Acc) ->
+                   case set_result(File, Result) of
+                       ok -> Acc;
+                       Error -> [Error | Acc]
+                   end
+                end,
+                [],
+                check_function(Function)).
 
 set_result(File, {error, Line, Text}) ->
     #{file => File,
       line => Line,
-      text => Text};
+      text => iolist_to_binary(Text)};
 set_result(_File, _) ->
     ok.
 
 check_function(FunctionNode) ->
-    % [{tree,clause,
-    %    {attr,[{text,"multi_fun"},{location,10}],[],none},
-    %    {clause,[{atom,[{text,"undefined"},{location,10}],undefined},
-    %             {var,[{text,"_Arg2"},{location,10}],'_Arg2'}],
-    %            none,
-    %            [{atom,[{text,"ok"},{location,11}],ok}]}},
-    % [(_a, b, _c), (_x, b, _c)]
-    % [[1, 0, 1], [1, 0, 1]] warning!
-    %
-    % [(a, _b, c), (_x, b, c)]
-    % [[0, 1, 0], [1, 0, 0]] ok
+    Line =
+        erl_anno:location(
+            erl_syntax:get_pos(FunctionNode)),
+    FuncName =
+        erl_syntax:atom_name(
+            erl_syntax:function_name(FunctionNode)),
+    FuncArity = erl_syntax:function_arity(FunctionNode),
+    FuncDesc = FuncName ++ "/" ++ integer_to_binary(FuncArity),
     Clauses = erl_syntax:function_clauses(FunctionNode),
-    ct:print("CLAUSES: ~p", [Clauses]),
-    Arguments =
-        lists:map(fun(Clause) ->
-                     Patterns = erl_syntax:clause_patterns(Clause),
-                     ct:print("PATTERNS: ~p", [Patterns]),
-                     lists:map(fun pattern_to_integer/1, Patterns)
-                  end,
-                  Clauses),
-    check_args(Arguments).
+    ComputedResults =
+        lists:foldl(fun(Clause, Result) ->
+                       Patterns = erl_syntax:clause_patterns(Clause),
+                       ClausePatterns = [pattern_to_integer(Pattern) || Pattern <- Patterns],
+                       check_unused_args(Result, ClausePatterns)
+                    end,
+                    [],
+                    Clauses),
+    check_computed_results(FuncDesc, Line, ComputedResults).
+
+%% @doc Computes position by position (multiply/and)
+%%      Will be 1 only when an argument is unused over all the function clauses
+check_unused_args([], Arguments) ->
+    Arguments;
+check_unused_args(Result, Arguments) ->
+    lists:zipwith(fun(A, B) -> A * B end, Result, Arguments).
 
 pattern_to_integer({var, [{text, ArgName}, {location, _Line}], _}) ->
     is_arg_ignored(ArgName);
@@ -54,8 +72,20 @@ is_arg_ignored("_" ++ _) ->
 is_arg_ignored(_) ->
     0.
 
-check_args(Arguments) ->
-    % @TODO WIP!
-    %% {error, Line, Text}
-    ct:print("ARGUMENT LIST: ~p", [Arguments]),
-    ok.
+check_computed_results(FuncDesc, Line, Results) ->
+    {_, Errors} =
+        lists:foldl(fun(Result, {Pos, Errors}) ->
+                       NewErrors =
+                           case Result of
+                               0 -> Errors;
+                               1 -> [set_error(Line, Pos, FuncDesc) | Errors]
+                           end,
+                       {Pos + 1, NewErrors}
+                    end,
+                    {1, []},
+                    Results),
+    Errors.
+
+set_error(Line, Pos, FuncDesc) ->
+    Text = io_lib:format("Param #~p is not used at '~s'", [Pos, FuncDesc]),
+    {error, Line, Text}.
