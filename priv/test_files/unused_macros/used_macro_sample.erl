3236036a62b04e25be375b5aeec6824612120f03
Brujo Benavides <elbrujohalcon@gmail.com>
2020-12-16 16:17:39 +0100

    Fix #7: Find unused macros

---
+++ priv/test_files/unused_macros/used_macro_sample.erl
@@ -0,0 +1,18 @@
+-module(used_macro_sample).
+
+-compile(export_all).
+
+-define(SIMPLE_MACRO, simple_macro).
+-define(MACRO_USED_IN(MACRO), ??MACRO).
+-define(MACRO_THAT_USES(OTHER_MACRO), {OTHER_MACRO, ?MACRO_USED_IN(?OTHER_MACRO)}).
+-define(USED_MACRO_WITH_BROKEN(CODE), case ?CODE of code -> ??CODE).
+
+using_macro(NotAMacro) ->
+    ?MACRO_THAT_USES(NotAMacro).
+
+simple_macro() -> ?SIMPLE_MACRO.
+
+broken() ->
+    ?USED_MACRO_WITH_BROKEN(SIMPLE_MACRO);
+    _ -> other
+end.
---
+++ src/rules/unused_macros.erl
@@ -0,0 +1,77 @@
+%% @doc A rule to detect unused macros.
+-module(unused_macros).
+
+-behaviour(hank_rule).
+
+-export([analyze/2]).
+
+%% @todo Extend the rule to check hrl files [https://github.com/AdRoll/rebar3_hank/issues/36]
+%% @todo Detect unparsable macros [https://github.com/AdRoll/rebar3_hank/issues/37]
+-spec analyze(hank_rule:asts(), hank_context:t()) -> [hank_rule:result()].
+analyze(FilesAndASTs, _Context) ->
+    [Result
+     || {File, AST} <- FilesAndASTs,
+        filename:extension(File) == ".erl",
+        Result <- do_analyze(File, AST)].
+
+do_analyze(File, AST) ->
+    FoldFun =
+        fun(Node, {Definitions, Usage}) ->
+           case erl_syntax:type(Node) of
+               attribute ->
+                   case hank_utils:attribute_name(Node) of
+                       define -> {[Node | Definitions], Usage};
+                       _ -> {Definitions, Usage}
+                   end;
+               macro -> {Definitions, [Node | Usage]};
+               _ -> {Definitions, Usage}
+           end
+        end,
+    {MacroDefinitions, MacroUsage} =
+        erl_syntax_lib:fold(FoldFun, {[], []}, erl_syntax:form_list(AST)),
+    DefinedMacros = lists:map(fun macro_definition_name/1, MacroDefinitions),
+    UsedMacros = lists:map(fun macro_application_name/1, MacroUsage),
+    [result(File, MacroName, MacroArity, MacroDefinitions)
+     || {MacroName, MacroArity} <- DefinedMacros -- UsedMacros].
+
+macro_definition_name(Node) ->
+    [MacroNameNode | _] = erl_syntax:attribute_arguments(Node),
+    case erl_syntax:type(MacroNameNode) of
+        application ->
+            MacroName =
+                erl_syntax:variable_literal(
+                    erl_syntax:application_operator(MacroNameNode)),
+            MacroArity = length(erl_syntax:application_arguments(MacroNameNode)),
+            {MacroName, MacroArity};
+        variable ->
+            {erl_syntax:variable_literal(MacroNameNode), none}
+    end.
+
+macro_application_name(Node) ->
+    MacroName =
+        erl_syntax:variable_literal(
+            erl_syntax:macro_name(Node)),
+    MacroArity =
+        case erl_syntax:macro_arguments(Node) of
+            none ->
+                none;
+            Args ->
+                length(Args)
+        end,
+    {MacroName, MacroArity}.
+
+result(File, MacroName, MacroArity, MacroDefinitions) ->
+    [Line] =
+        [erl_anno:location(
+             erl_syntax:get_pos(Md))
+         || Md <- MacroDefinitions, macro_definition_name(Md) == {MacroName, MacroArity}],
+    Text =
+        case MacroArity of
+            none ->
+                iolist_to_binary(io_lib:format("?~s is unused", [MacroName]));
+            MacroArity ->
+                iolist_to_binary(io_lib:format("?~s/~p is unused", [MacroName, MacroArity]))
+        end,
+    #{file => File,
+      line => Line,
+      text => Text}.
