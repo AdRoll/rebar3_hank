4f4e8ef40058b27b5131f25472cc8cd3a03669c1
Pablo Brud <pbrudnick@gmail.com>
2020-12-21 11:08:23 -0300

    Update src/rules/single_use_hrl_attrs.erl
    Co-authored-by: Brujo Benavides <fernando.benavides@adroll.com>

--- src/rules/single_use_hrl_attrs.erl
+++ src/rules/single_use_hrl_attrs.erl
@@ -11,7 +11,7 @@
 -export([analyze/2]).
 
 %% @doc This builds a list of header files with its attributes.
-%%      Then traverse through the files ASTs mapping their macros and records
+%%      Then traverse the file ASTs mapping their macros and records
 %%      And check where they were used just once
 -spec analyze(hank_rule:asts(), hank_context:t()) -> [hank_rule:result()].
 analyze(FilesAndASTs, _Context) ->
