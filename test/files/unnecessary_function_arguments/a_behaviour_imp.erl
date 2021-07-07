188bd7610c5cff919211be6b33d25029f9b3ecb3
Brujo Benavides <fernando.benavides@adroll.com>
2021-07-07 16:58:25 +0200

    Fix #143: Analyze private function on unknown/dynamic behavior implementations (#147)
    * Fix #143: Analyze private function on unknown/dynamic behavior implementations
    
    * Clarification
    
    * Update test/files/unnecessary_function_arguments/a_behaviour_imp.erl
    
    Co-authored-by: Pablo Brud <pbrudnick@gmail.com>
    
    Co-authored-by: Pablo Brud <pbrudnick@gmail.com>

--- test/files/unnecessary_function_arguments/a_behaviour_imp.erl
+++ test/files/unnecessary_function_arguments/a_behaviour_imp.erl
@@ -1,5 +1,6 @@
 %% @doc This module implements a local behaviour:
-%%      The unnecessary_function_arguments rule will be ignored for the whole file.
+%%      The unnecessary_function_arguments rule will be ignored for all
+%%      exported functions since we can't tell which ones are dynamic callbacks.
 -module(a_behaviour_imp).
 
 -behaviour(a_behaviour).
@@ -13,6 +14,11 @@ a_kind_of_magic(_) ->
     % this function won't be warned since it's a callback
     implemented.
 
-%% this won't warn because the whole file implementing a local behaviour is ignored
+%% this exported function won't warn because the module implements a local behaviour
 function_with_ignored_arg(_, Value) ->
+    non_exported_function_with(ignored_arg, Value).
+
+%% this should produce a warning since, being a non-exported function, it can't
+%% be a callback implementation.
+non_exported_function_with(_IgnoredArg, Value) ->
     Value.
